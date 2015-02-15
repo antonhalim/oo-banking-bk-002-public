7a7970a1d44a52cfd12f2f67487f314366499e0d
axton21 <axton_n@yahoo.com>
2015-02-15 18:05:23 -0500

    done

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,34 @@
 class BankAccount
-  # code here
+  require 'pry'
+  attr_reader :name
+  attr_accessor :balance, :status, :deposit
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(money)
+    @balance += money
+  end
+
+  def display_balance
+    "Your Balance is $#{self.balance}."
+  end
+
+  def valid?
+    if self.status == "open" && self.balance > 0
+      true
+    else
+      false
+    end
+  end
+
+  def close_account
+    self.status = "closed"
+  end
+
+
+
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,43 @@
 class Transfer
-  # code here
+  require 'pry'
+  attr_accessor :amount, :status, :sender, :receiver
+
+  def initialize(sender, receiver, amount=50)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+    @both_valid = false
+  end
+
+  def both_valid?
+    if sender.valid? && receiver.valid? == true
+     @both_valid = true
+   else
+     @both_valid = false
+   end
+ end
+
+ def execute_transaction
+  if self.status == "pending" && self.both_valid? == true && sender.balance > @amount
+    sender.balance -= 50
+    receiver.balance += 50
+    self.status = "complete"
+  else
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
+end
+
+def reverse_transfer
+  if self.status == "complete" && self.both_valid? == true && sender.balance > @amount
+    sender.balance += 50
+    receiver.balance -= 50
+    self.status = "reversed"
+  else
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end 
+end
+
 end
